553de2586feafe3620e15b2b49a2330d915ccbeb
arthurxuwei <diandianx007@gmail.com>
2014-08-28 18:21:25 +0800

    EA

--- poj1006.cpp
+++ poj1006.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int arr1[924];
-int arr2[759];
-int arr3[644];
+int arr1[925];
+int arr2[760];
+int arr3[645];
 
 int has(int a[], int size, int v) {
 	int left = 0, right = size - 1;
@@ -25,28 +25,28 @@ int main() {
 	while(cin >> p >> e >> t >> d) {
 		n++;
 		if (p == -1) break;
-		for(int i = 0; i<924; ++i){
-			arr1[i] = 23 *(i+1) +p;
+		for(int i = 0; i<925; ++i){
+			arr1[i] = 23 * i +(p%23);
 			if (arr1[i] > 21252) {
 				arr1[i] = 21252;break;
 			}
 		}
-		for(int i = 0; i<759; ++i){
-			arr2[i] = 28 *(i+1) + e;
+		for(int i = 0; i<760; ++i){
+			arr2[i] = 28 *i + e%28;
 			if (arr2[i] > 21252) {
 				arr2[i] = 21252;break;
 			}
 		}
-		for(int i=0; i<644; ++i){
-			arr3[i] = 33 * (i+1) + t;
+		for(int i=0; i<645; ++i){
+			arr3[i] = 33 * i + t%33;
 			if (arr3[i] > 21252) {
 				arr3[i] = 21252;break;
 			}
 		}
 
-		for (int i = 0; i < 924; ++i) {
+		for (int i = 0; i < 925; ++i) {
 //			cout << has(arr2, 759, arr1[i]) << has(arr3, 644, arr1[i]) << endl;
-			if( has(arr2, 759, arr1[i]) && has(arr3, 644, arr1[i]) && arr1[i] >= d) {
+			if( has(arr2, 760, arr1[i]) && has(arr3, 645, arr1[i]) && arr1[i] > d) {
 				cout << "Case " << n << ": the next triple peak occurs in " << arr1[i] - d << " days." << endl;
 				break;
 			}
