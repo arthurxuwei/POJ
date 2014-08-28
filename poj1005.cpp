6a23dfdd707c1ef49e9f69e1e6182fff21747fbd
arthurxuwei <diandianx007@gmail.com>
2014-08-28 15:00:36 +0800

    WA

--- poj1005.cpp
+++ poj1005.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 float PI = 3.14;
 
-double getR(int year) {
-	return sqrt(year*100/PI);
-}
-
 int main () {
 	int n;
 	cin >> n;
 	for(int i=0; i<n;i++) {
 		double a,b;
 		cin >> a >> b;
-		for (int j=1;j<500; j++) {
-			double r = getR(j);
-			if (r>=a && r>=b) {
-				cout << "Property " << i+1 << ": This property will begin eroding in year " << j <<"." << endl;
-				break;
-			}
-		}
+		int j = PI *  (a*a + b*b) / 100;
+		cout << "Property " << i+1 << ": This property will begin eroding in year " << j+1 <<"." << endl;
 	}
 	cout << "END OF OUTPUT." << endl;
 }
