f6964058651830ec49bd8b1c40005897457a7e95
arthurxuwei <diandianx007@gmail.com>
2014-08-28 17:54:43 +0800

    TL

--- poj1006.cpp
+++ poj1006.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
 using namespace std;
 
+int arr1[924];
+int arr2[759];
+int arr3[644];
+
+int has(int a[], int size, int v) {
+	int left = 0, right = size - 1;
+	int m;
+	while (left <= right) {
+		m = (left + right)/2;
+		if ( a[m] > v ) {
+			right = m - 1;
+		} else if (a[m] < v) {
+			left = m + 1;
+		} else return 1;
+	}
+	return 0;
+}
+
 int main() {
 	int p,e,t,d;
-	int t1, t2, t3;
-	int n = 0;
+	int n=0;
 	while(cin >> p >> e >> t >> d) {
 		n++;
 		if (p == -1) break;
-		int find = 0;	
 		for(int i = 0; i<924; ++i){
-			if (find) break;
-			t1 = 23 *(i+1) + p;
-			if(t1 >=21252) {
-				t1 = 21252;
+			arr1[i] = 23 *(i+1) +p;
+			if (arr1[i] > 21252) {
+				arr1[i] = 21252;break;
 			}
-			
-			for(int i = 0; i<759; ++i){
-				if (find) break;
-				t2 = 28 *(i+1) + e;
-				if(t2 >=21252) {
-					t2 = 21252;
-				}
-				for(int i=0; i<644; ++i){
-					if (find) break;
-					t3 = 33 * (i+1) + t;
-					if(t3 >=21252) {
-						t3 = 21252;
-					}
+		}
+		for(int i = 0; i<759; ++i){
+			arr2[i] = 28 *(i+1) + e;
+			if (arr2[i] > 21252) {
+				arr2[i] = 21252;break;
+			}
+		}
+		for(int i=0; i<644; ++i){
+			arr3[i] = 33 * (i+1) + t;
+			if (arr3[i] > 21252) {
+				arr3[i] = 21252;break;
+			}
+		}
 
-					if(t1 == t2 && t2 == t3 && t1 >= d) {
-						find = 1;
-						cout << "Case " << n << ": the next triple peak occurs in " << t1 - d << " days." << endl;
-					} 
-				}
-			}	
+		for (int i = 0; i < 924; ++i) {
+//			cout << has(arr2, 759, arr1[i]) << has(arr3, 644, arr1[i]) << endl;
+			if( has(arr2, 759, arr1[i]) && has(arr3, 644, arr1[i]) && arr1[i] >= d) {
+				cout << "Case " << n << ": the nex triple peak occurs in " << arr1[i] - d << " days." << endl;
+				break;
+			}
 		}
 	}
 	
