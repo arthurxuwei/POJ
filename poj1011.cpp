918685e1403601c30d1f0f62c05c23655d576c3e
arthurxuwei <diandianx007@gmail.com>
2014-09-05 16:07:31 +0800

    EA

--- poj1011.cpp
+++ poj1011.cpp
@@ -1,27 +1,54 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+int a[100];
+bool vis[100];
 
+int cmp(const void *a, const void *b) {
+	return *(int *)b-*(int *)a;
+}
+
+int dfs(int len, int n, int stick, int ret){
+	if(stick==n && ret==0)
+		return len;
+	if(ret==0)
+		ret = len;
+	int i;
+	for(i=0;i<n;i++) {
+		if(vis[i] == true) continue;
+		if(a[i]>ret) continue;
+		vis[i] = true;
+		if(dfs(len, n, stick+1, ret-a[i])) return len;
+		vis[i] = false;
+		if(ret == a[i] || len == ret) break;
+		while(a[i] == a[i+1]) ++i;
+	}
+	return 0;
+}
 main() {
 	int len;
+	int i,j,k;
 	while(cin >> len) {
 		if(len==0){
 			return 0;
 		}
-		int a[len];
 		int max=0;
-		int sum=0;
+		int sum = 0;
 		int tmp;
-		for(int i=0; i<len;i++) {
+		for(int x=0; x<len;x++) {
 			cin >> tmp;
 			if(tmp > max) max = tmp;
 			sum+=tmp;
-			a[i] = tmp;
+			a[x] = tmp;
 		}
-		for(int i=0; i<50; i++) {
-			if (sum%(max+i) == 0) {
-				cout << max+i << endl;
-				break;
-			}
+		qsort(a, len, sizeof(int), cmp);
+		for(i = max; i <= sum; ++i) {
+			if(sum%i!=0) continue;
+			for(j=0; j<len; ++j) 
+				vis[j] = false;
+			k = dfs(i, len, 0, i);
+			if(k!=0) break;
 		}
+		cout << k << endl;
 	}
 }
