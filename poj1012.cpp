266a2ebca869774846ebce73de741796b6f3e72f
arthurxuwei <diandianx007@gmail.com>
2014-09-06 16:34:31 +0800

    TLA

--- poj1012.cpp
+++ poj1012.cpp
@@ -2,51 +2,33 @@
 
 using namespace std;
 
-int joseph(int a[], int n,int m) {
+int temp[13] = {0};
+int joseph(int k) {
 	
-	int k =1;
-	int i = -1;
-
-	while(1) {
-		for(int j=0; j <m;) {
-			i = (i+1) %n;
-			if(a[i] != 0)
-				j++;		
+	int n =2*k;
+	int ans[30] = {0};
+	int m = 1;
+	for(int i=1;i<=n/2;i++){
+		ans[i] = (ans[i-1]+m-1)%(n-i+1);
+		if(ans[i] <k) {
+			i=0;
+			m++;
 		}
-		if(k<=n/2 && a[i] <= n/2) return 0;
-		if(k==n) break;
-	//	cout << a[i] << ",";
-		a[i] = 0;
-		k++;
 	}
-	//cout << a[i] << endl;
-	return 1;
-}
-int factorial(int n) {
-	int t = 1;
-	for(int i=n; i>0; i--){
-		t *= i;
-	}
-	return t;
+	temp[k] = m;
+	return m;
 }
 
 main() {
 	int n;
+
 	while(true) {
 		cin >> n;
 		if(n == 0) break;
-		int a[2*n];
-		int times= factorial(2*n);
-	//	cout << "times: " << times << endl;
-
-		for(int i=n; i<times; i++){
-			if( i%(2*n) < n ) continue;
-			for(int j=0; j<2*n; j++)
-				a[j] = j+1;
-			if(joseph(a, 2*n, i+1)){
-				cout << i+1 << endl;
-				break;
-			}
+		if(temp[n]) {
+			cout << temp[n] << endl;
+			continue;
 		}
+		cout <<	joseph(n) << endl;
 	}
 }
